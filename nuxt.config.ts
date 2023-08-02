654c11cc196854c6056e5e0398e648d752a820b8
Huge Letters <ckuka71@gmail.com>
2023-08-02 11:43:31 +0200

    fixed prod detection in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // for svg modules import type declarations
 import 'vite-svg-loader';
 
+const isDev = process.env.NODE_ENV !== 'production';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
    modules: [
@@ -17,8 +19,8 @@ export default defineNuxtConfig({
    vite: {},
    plausible: {
       // Events will by sent to {apiHost}/api/event
-      apiHost: process.dev ? `http://localhost:3000/` : undefined,
-      trackLocalhost: process.dev,
+      apiHost: isDev ? `http://localhost:3000/` : undefined,
+      trackLocalhost: isDev,
    },
    tailwindcss: {},
    googleFonts: { families: { Inter: [400, 500, 600, 700] } },
